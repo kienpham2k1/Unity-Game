ae743f3903c5906bd8331828e2812d013ed9e29d
Kien <phamthanhkien2k1@gmail.com>
2023-02-10 10:21:13 +0700

    Enemy obj

---
+++ Unity Game/Assets/Scrips/EnemyAI.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(BoxCollider2D))]
+public class EnemyAI : MonoBehaviour
+{
+    //Reference to waypoints
+    public List<Transform> points;
+    //The int value for next point index
+    public int nextID = 0;
+    //The value of that applies to ID for changing
+    int idChangeValue = 1;
+    //Speed of movement or flying
+    public float speed = 2;
+
+
+    private void Reset()
+    {
+        Init();
+    }
+
+    void Init()
+    {
+        GetComponent<BoxCollider2D>().isTrigger = true;
+
+        GameObject root = new GameObject(name + "_Root");
+        root.transform.position = transform.position;
+        transform.SetParent(root.transform);
+        GameObject waypoints = new GameObject("Waypoints");
+        waypoints.transform.SetParent(root.transform);
+        waypoints.transform.position = root.transform.position;
+        GameObject p1 = new GameObject("Point1"); p1.transform.SetParent(waypoints.transform); p1.transform.position = root.transform.position;
+        GameObject p2 = new GameObject("Point2"); p2.transform.SetParent(waypoints.transform); p2.transform.position = root.transform.position;
+
+        points = new List<Transform>();
+        points.Add(p1.transform);
+        points.Add(p2.transform);
+    }
+
+    private void Update()
+    {
+        MoveToNextPoint();
+    }
+
+    void MoveToNextPoint()
+    {
+        Transform goalPoint = points[nextID];
+        if (goalPoint.transform.position.x > transform.position.x)
+            transform.localScale = new Vector3(-1, 1, 1);
+        else
+            transform.localScale = new Vector3(1, 1, 1);
+        transform.position = Vector2.MoveTowards(transform.position, goalPoint.position, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, goalPoint.position) < 0.2f)
+        {
+            if (nextID == points.Count - 1)
+                idChangeValue = -1;
+            if (nextID == 0)
+                idChangeValue = 1;
+            nextID += idChangeValue;
+        }
+    }
+}
